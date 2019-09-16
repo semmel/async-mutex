50326ff2038a46820d46287ed0c1b617157f31bd
Matthias Seemann <296476+semmel@users.noreply.github.com>
2019-09-16 21:48:50 +0200

    Added missing Error class

---
+++ src/MutexTimeoutError.js
@@ -0,0 +1,23 @@
+/**
+ * with-timeout: MutexTimeoutError.js
+ *
+ * Created by Matthias Seemann on 13.09.2019.
+ * Copyright (c) 2019 Visisoft OHG. All rights reserved.
+ */
+
+class MutexTimeoutError extends Error
+{
+	constructor() {
+		// Pass remaining arguments (including vendor specific ones) to parent constructor
+		super("An async mutex has timed out");
+		
+		// Maintains proper stack trace for where our error was thrown (only available on V8)
+		if (Error.captureStackTrace) {
+			Error.captureStackTrace(this, MutexTimeoutError);
+		}
+		
+		this.name = 'MutexTimeoutError';
+	}
+}
+
+module.exports = MutexTimeoutError;
